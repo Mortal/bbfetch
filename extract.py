e34f0cfa4f6c23d7513bc5ae2fe4d6a7f436a2d3
Mathias Rav <rav@cs.au.dk>
2015-02-17 09:53:37 +0100

    Work on extract.py

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 import json
 import zipfile
 import argparse
@@ -32,16 +33,112 @@ FILES_FIELD = re.compile(
 EXTENSIONS = ['.pdf']
 
 
+def guess_params():
+    directory = os.path.expanduser('~/Downloads')
+    filenames = os.listdir(directory)
+    filenames = [
+        os.path.join(directory, f)
+        for f in filenames
+        if f.startswith('gradebook_BB') and f.endswith('.zip')
+    ]
+    filenames.sort(key=lambda f: os.stat(f).st_mtime)
+    filename = filenames[-1]
+    print("File: %s" % filename)
+    attempt = int(input("Attempt: "))
+    return filename, attempt
+
+
+def print_handin_info(i, group, data):
+    try:
+        with open(data['comments_file']) as fp:
+            comments_size = os.fstat(fp.fileno()).st_size
+            comments = '%d B comments' % comments_size
+            first_line = fp.readline().strip()
+    except FileNotFoundError:
+        comments = 'no comments'
+        first_line = ''
+    annotated_file = os.path.splitext(data['file'])[0] + '.pep'
+    if os.path.exists(annotated_file):
+        annotations = 'annotated'
+    else:
+        annotations = 'not annotated'
+    print("%d. %s (%s; %s) %s" % (i + 1, group, comments, annotations, first_line))
+
+
+def previous_handin(group, data):
+    handin = os.path.dirname(data['file'])
+    o = re.match(r'(.*_)(\d+)', handin)
+    print(o)
+    if o:
+        directory = o.group(1) + str(int(o.group(2)) - 1)
+        print(directory)
+        base = os.path.join(directory, group)
+        annotated = base + '_handin_ann.pdf'
+        print(annotated)
+        if os.path.exists(annotated):
+            return annotated
+
+
+def handin_loop(i, handin):
+    group, data = handin
+    previous = previous_handin(group, data)
+    print('')
+    while True:
+        print_handin_info(i, group, data)
+        print("0. Back")
+        print("1. Annotate and edit comments")
+        if previous:
+            print("2. Open annotated previous version")
+
+        # Don't catch KeyboardInterrupt here
+        j = int(input())
+
+        if j == 1:
+            subprocess.call(('pdfa', data['file']))
+            subprocess.call(('vim', data['comments_file']))
+        elif j == 2 and previous:
+            subprocess.call(('xdg-open', previous))
+        else:
+            break
+
+
+def grade_loop(handins):
+    while True:
+        for i, (group, data) in enumerate(handins):
+            print_handin_info(i, group, data)
+        try:
+            i = int(input()) - 1
+            if 0 <= i < len(handins):
+                handin_loop(i, handins[i])
+        except KeyboardInterrupt:
+            break
+
+
+def print_comments(handins):
+    for group, data in handins:
+        print('%s' % (group,))
+        try:
+            with open(data['comments_file']) as fp:
+                print(fp.read())
+        except FileNotFoundError:
+            print("no comments\n")
+
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--all', action='store_true')
-    parser.add_argument('filename')
-    parser.add_argument('attempt', type=int, default=1)
-    args = parser.parse_args()
+    args = sys.argv[1:]
+    if len(args) == 0:
+        zipname, attempt = guess_params()
+    elif len(args) == 1:
+        zipname = args[0]
+        attempt = 1
+    else:
+        zipname, attempt = args
+
+    reject_invalid = True
 
     handins = {}
 
-    with zipfile.ZipFile(args.filename, 'r') as zf:
+    with zipfile.ZipFile(zipname, 'r') as zf:
         for zinfo in zf.infolist():
             filename = zinfo.filename
             o = re.fullmatch(FILENAME, filename)
@@ -53,7 +150,7 @@ def main():
             d = o.groupdict()
 
             d['handin'] = re.sub('^Aflevering ', '', d['handin'])
-            d['handin'] += '_%d' % args.attempt
+            d['handin'] += '_%d' % attempt
             d['group'] = re.sub('^Gruppe ([A-Z]*\d+) - ', r'\1-',
                                 d['group']).replace(' ', '0')
             # d['suffix'] = re.sub('[^0-9A-Za-z_.-]+', '_', d['suffix'])
@@ -88,7 +185,7 @@ def main():
                 #             print("%s:\n%s" % (k, v.decode()))
                 #     print('')
             else:
-                if not args.all and d['extension'] not in EXTENSIONS:
+                if reject_invalid and d['extension'] not in EXTENSIONS:
                     print("Rejecting %r" % d['suffix'])
                 else:
                     handin['file'] = output_base + '_handin' + d['extension']
@@ -101,14 +198,8 @@ def main():
                         print("skipping %r" % handin['file'])
 
     handins = sorted(handins.items(), key=lambda x: x[0])
-    while True:
-        for i, (group, data) in enumerate(handins):
-            print("%d. %s" % (i + 1, group))
-        i = int(input()) - 1
-        if 0 <= i < len(handins):
-            group, data = handins[i]
-            subprocess.call(('pdfa', data['file']))
-            subprocess.call(('vim', data['comments_file']))
+    grade_loop(handins)
+    print_comments(handins)
 
 
 if __name__ == "__main__":
