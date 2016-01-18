b4a82f3584470243dde8b25c76d2a9685883b7b6
Mathias Rav <rav@cs.au.dk>
2016-01-18 11:35:00 +0100

    More work on parsing visit stats

--- fetch.py
+++ fetch.py
@@ -197,15 +197,50 @@ def main():
     session = requests.Session()
     requests.cookies.merge_cookies(session.cookies, cookies)
     course_id = args.course
+
+    table = get_visit_stats(session, course_id, get_auth)
+    with open('visit_stats.txt', 'a') as fp:
+        for name, time in table:
+            fp.write('%s %s\n' % (time, name))
+
+    requests.cookies.merge_cookies(cookies, session.cookies)
+    cookies.save(ignore_discard=True)
+
+
+def get_visit_stats(session, course_id, get_auth):
     url = (
         'https://bb.au.dk/webapps/blackboard/content/manageDashboard.jsp?' +
         'course_id=%s&showAll=true&sortCol=LastLoginCol&sortDir=D' % course_id)
     r = autologin(session, session.get(url), get_auth)
-    with open('tmp.html', 'wb') as fp:
-        fp.write(r.content)
     document = html5lib.parse(r.content, encoding=r.encoding)
-    requests.cookies.merge_cookies(cookies, session.cookies)
-    cookies.save(ignore_discard=True)
+    keys, rows = parse_visit_stats(document)
+    first = keys.index('FirstNameCol')
+    last = keys.index('LastNameCol')
+    time = keys.index('LastLoginCol')
+    data = [('%s %s' % (r[first], r[last]), r[time]) for r in rows]
+    return data
+
+
+def parse_visit_stats(document):
+    table = document.find('.//h:table[@id="listContainer_datatable"]', NS)
+    header = table.find('./h:thead', NS)
+    keys = []
+    for h in header[0]:
+        text = ' '.join(''.join(h.itertext()).split())
+        sortheader = h.find('./h:a[@class="sortheader"]', NS)
+        if sortheader:
+            mo = re.search(r'sortCol=([^&]*)', sortheader.get('href'))
+            if mo:
+                text = mo.group(1)
+        keys.append(text)
+    rows = table.findall('./h:tbody/h:tr', NS)
+    res = []
+    for row in rows:
+        r = []
+        res.append(r)
+        for cell in row:
+            r.append(' '.join(''.join(cell.itertext()).split()))
+    return keys, res
 
 
 if __name__ == "__main__":
