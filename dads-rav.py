c13c705d6160653cf4811f93eb99afbd179f52d7
Mathias Rav <rav@cs.au.dk>
2016-04-21 09:59:17 +0200

    Add more documentation to dads-rav.py

--- dads-rav.py
+++ dads-rav.py
@@ -1,3 +1,4 @@
+import os
 import re
 from blackboard.grading import Grading
 
@@ -5,10 +6,12 @@ from blackboard.grading import Grading
 class GradingDads(Grading):
     @classmethod
     def get_username(cls, args):
+        """Username to log in to BlackBoard with."""
         return '20103940'
 
     @classmethod
     def get_course(cls, args):
+        """BlackBoard course ID including leading "_" and trailing "_1"."""
         return '_49446_1'
 
     def get_group_name_display(self, group_name):
@@ -16,6 +19,7 @@ class GradingDads(Grading):
         if group_name is None:
             return group_name
         elif group_name.startswith('Gruppe'):
+            # Translate "Gruppe DAx - yy" into "DAx-yy"
             x = group_name.split()
             return '%s-%s' % (x[1], x[3])
         else:
@@ -26,8 +30,10 @@ class GradingDads(Grading):
         Return True if the given student should be visible,
         that is, if it is a member of the user's TA class.
         """
+        # Add your classes to the following list, e.g. ["DA1", "DA2"]
+        classes = ['2']
         group_name = self.get_group_name_display(student.group) or ''
-        return group_name.startswith('2-')
+        return any(group_name.startswith(c + '-') for c in classes)
 
     def get_student_ordering(self, student):
         """
@@ -42,7 +48,8 @@ class GradingDads(Grading):
         Return an abbreviation of the name of an assignment.
         """
         if assignment.name.startswith('Aflevering'):
-            return assignment.name.split()[-1]
+            # Translate "Aflevering 3" into "3"
+            return assignment.name.split()[1]
         else:
             return assignment.name
 
@@ -52,21 +59,24 @@ class GradingDads(Grading):
         relating to the given handin.
         """
 
+        # expanduser translates "~" into your home directory
+        base = os.path.expanduser('~/TA/dADS2-2016')
+
         group_name = attempt.group_name
         if group_name.startswith('Gruppe'):
+            # Translate "Gruppe DAx - yy" into "DAx" and "yy"
             class_name = group_name.split()[1]
             group_number = group_name.split()[3]
         else:
             class_name = group_number = None
 
-        attempt_id = attempt.id
-        if attempt_id.startswith('_'):
-            attempt_id = attempt_id[1:]
-        if attempt_id.endswith('_1'):
-            attempt_id = attempt_id[:-2]
+        attempt_id = re.sub(r'_(.*)_1', r'\1', attempt.id)
+        assignment = self.get_assignment_name_display(attempt.assignment)
+
+        # Translate Handin 1, Class DA2, Group 3, attempt id 456 into
+        # ~/TA/dADS2-2016/A1-DA2/03-456
         return '{base}/A{assignment}-{class_name}/{group}_{id}'.format(
-            base='/home/rav/TA/dADS2-2016',
-            assignment=self.get_assignment_name_display(attempt.assignment),
+            base=base, assignment=assignment,
             class_name=class_name, group=group_number, id=attempt_id)
 
     def get_feedback_score(self, comments):
