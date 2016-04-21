1db48c1f1e93fa0a9406a51b7da70794442d88a2
Mathias Rav <rav@cs.au.dk>
2016-04-21 10:17:30 +0200

    Move submit_grade into backend.py

--- blackboard/backend.py
+++ blackboard/backend.py
@@ -1,3 +1,5 @@
+import io
+import os
 import json
 import html5lib
 
@@ -132,3 +134,84 @@ def fetch_attempt(session, attempt_id):
         submission=submission_text,
         comments=comments,
         files=files)
+
+
+def submit_grade(session, attempt_id, grade, text, filenames):
+    assert isinstance(session, BlackBoardSession)
+    url = (
+        'https://bb.au.dk/webapps/assignment/gradeAssignmentRedirector' +
+        '?course_id=%s' % session.course_id +
+        '&groupAttemptId=%s' % attempt_id)
+    # We need to fetch the page to get the nonce
+    response = session.get(url)
+    document = html5lib.parse(response.content, encoding=response.encoding)
+    form = document.find('.//h:form[@id="currentAttempt_form"]', NS)
+    if form is None:
+        raise ParserError("No <form id=currentAttempt_form>", response)
+    fields = (form.findall('.//h:input', NS) +
+              form.findall('.//h:textarea', NS))
+    data = [
+        (field.get('name'), field.get('value'))
+        for field in fields
+        if field.get('name')
+    ]
+    data_lookup = {k: i for i, (k, v) in enumerate(data)}
+
+    def data_get(k, *args):
+        if args:
+            d, = args
+        try:
+            return data[data_lookup[k]][1]
+        except KeyError:
+            if args:
+                return d
+            raise
+
+    def data_set(k, v):
+        try:
+            data[data_lookup[k]] = k, v
+        except KeyError:
+            data_lookup[k] = len(data)
+            data.append((k, v))
+
+    def data_extend(kvs):
+        for k, v in kvs:
+            data_lookup[k] = len(data)
+            data.append((k, v))
+
+    data_set('grade', str(grade))
+    data_set('feedbacktext', text)
+
+    files = []
+
+    for i, filename in enumerate(filenames):
+        base = os.path.basename(filename)
+        data_extend([
+            ('feedbackFiles_attachmentType', 'L'),
+            ('feedbackFiles_fileId', 'new'),
+            ('feedbackFiles_artifactFileId', 'undefined'),
+            ('feedbackFiles_artifactType', 'undefined'),
+            ('feedbackFiles_artifactTypeResourceKey', 'undefined'),
+            ('feedbackFiles_linkTitle', base),
+        ])
+        with open(filename, 'rb') as fp:
+            fdata = fp.read()
+        files.append(('feedbackFiles_LocalFile%d' % i, (base, fdata)))
+    post_url = (
+        'https://bb.au.dk/webapps/assignment//gradeGroupAssignment/submit')
+    if not files:
+        # BlackBoard requires the POST to be
+        # Content-Type: multipart/form-data.
+        # Unfortunately, requests can only make a form-data POST
+        # if it has file-like input in the files list.
+        files = [('dummy', io.StringIO(''))]
+    try:
+        response = session.post(post_url, data=data, files=files)
+    except:
+        logger.exception("data=%r files=%r", data, files)
+        raise
+    document = html5lib.parse(response.content, encoding=response.encoding)
+    msg = document.find('.//h:span[@id="goodMsg1"]', NS)
+    if msg is None:
+        raise ParserError("No goodMsg1 in POST response", response)
+    logger.debug("goodMsg1: %s", element_text_content(msg))
--- blackboard/grading.py
+++ blackboard/grading.py
@@ -13,7 +13,7 @@ from blackboard.gradebook import (
     Gradebook, Attempt, truncate_name, StudentAssignment)
 from blackboard.elementtext import (
     element_to_markdown, element_text_content)
-from blackboard.backend import fetch_attempt
+from blackboard.backend import fetch_attempt, submit_grade
 
 
 NS = {'h': 'http://www.w3.org/1999/xhtml'}
@@ -264,87 +264,6 @@ class Grading(blackboard.Serializable):
         elif accept:
             return 1
 
-    def submit_grade(self, attempt_id, grade, text, filenames):
-        if isinstance(attempt_id, Attempt):
-            attempt_id = attempt_id.id
-        url = (
-            'https://bb.au.dk/webapps/assignment/gradeAssignmentRedirector' +
-            '?course_id=%s' % self.session.course_id +
-            '&groupAttemptId=%s' % attempt_id)
-        # We need to fetch the page to get the nonce
-        response = self.session.get(url)
-        document = html5lib.parse(response.content, encoding=response.encoding)
-        form = document.find('.//h:form[@id="currentAttempt_form"]', NS)
-        if form is None:
-            raise ParserError("No <form id=currentAttempt_form>", response)
-        fields = (form.findall('.//h:input', NS) +
-                  form.findall('.//h:textarea', NS))
-        data = [
-            (field.get('name'), field.get('value'))
-            for field in fields
-            if field.get('name')
-        ]
-        data_lookup = {k: i for i, (k, v) in enumerate(data)}
-
-        def data_get(k, *args):
-            if args:
-                d, = args
-            try:
-                return data[data_lookup[k]][1]
-            except KeyError:
-                if args:
-                    return d
-                raise
-
-        def data_set(k, v):
-            try:
-                data[data_lookup[k]] = k, v
-            except KeyError:
-                data_lookup[k] = len(data)
-                data.append((k, v))
-
-        def data_extend(kvs):
-            for k, v in kvs:
-                data_lookup[k] = len(data)
-                data.append((k, v))
-
-        data_set('grade', str(grade))
-        data_set('feedbacktext', text)
-
-        files = []
-
-        for i, filename in enumerate(filenames):
-            base = os.path.basename(filename)
-            data_extend([
-                ('feedbackFiles_attachmentType', 'L'),
-                ('feedbackFiles_fileId', 'new'),
-                ('feedbackFiles_artifactFileId', 'undefined'),
-                ('feedbackFiles_artifactType', 'undefined'),
-                ('feedbackFiles_artifactTypeResourceKey', 'undefined'),
-                ('feedbackFiles_linkTitle', base),
-            ])
-            with open(filename, 'rb') as fp:
-                fdata = fp.read()
-            files.append(('feedbackFiles_LocalFile%d' % i, (base, fdata)))
-        post_url = (
-            'https://bb.au.dk/webapps/assignment//gradeGroupAssignment/submit')
-        if not files:
-            # BlackBoard requires the POST to be
-            # Content-Type: multipart/form-data.
-            # Unfortunately, requests can only make a form-data POST
-            # if it has file-like input in the files list.
-            files = [('dummy', io.StringIO(''))]
-        try:
-            response = self.session.post(post_url, data=data, files=files)
-        except:
-            logger.exception("data=%r files=%r", data, files)
-            raise
-        document = html5lib.parse(response.content, encoding=response.encoding)
-        msg = document.find('.//h:span[@id="goodMsg1"]', NS)
-        if msg is None:
-            raise ParserError("No goodMsg1 in POST response", response)
-        logger.debug("goodMsg1: %s", element_text_content(msg))
-
     def upload_all_feedback(self, dry_run=False):
         uploads = []
         attempts = self.get_attempts(needs_upload=True)
@@ -372,7 +291,7 @@ class Grading(blackboard.Serializable):
             print(uploads)
         else:
             for attempt, score, feedback, attachments in uploads:
-                self.submit_grade(attempt, score, feedback, attachments)
+                submit_grade(attempt.id, score, feedback, attachments)
 
     def main(self, args, session, grading):
         if args.refresh:
