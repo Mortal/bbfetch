e8a86156796f1b98de3bdc464bdc2a68d75571c0
Mathias Rav <rav@cs.au.dk>
2016-04-26 14:42:51 +0200

    Adjust roberto-dSik/grading.py to match rav-dADS2/grading.py

--- roberto-dSik/grading.py
+++ roberto-dSik/grading.py
@@ -16,23 +16,25 @@ class Grading(blackboard.grading.Grading):
         """BlackBoard course ID including leading "_" and trailing "_1"."""
         return '_49454_1'
 
-    def get_group_name_display(self, group_name):
-        """Given a group name, compute an abbreviation of the name."""
-        if group_name is None:
-            return group_name
-        elif group_name.startswith('Gruppe'):
-            x = group_name.split()
-            return '%s-%s' % (x[1], x[3])
-        else:
-            return group_name
+    def get_student_group_display(self, student):
+        for g in self.get_student_groups(student):
+            if g.name.startswith('Gruppe'):
+                x = group_name.split()
+                return '%s-%s' % (x[1], x[3])
+        return ''
 
     def get_student_visible(self, student):
         """
         Return True if the given student should be visible,
         that is, if it is a member of the user's TA class.
         """
-        group_name = self.get_group_name_display(student.group) or ''
-        return group_name.startswith('DA1-') or group_name.startswith('DA2-')
+        # Add your classes to the following list, e.g. ["DA1", "DA2"]
+        classes = ['DA1', 'DA2']
+        for g in self.get_student_groups(student):
+            for c in classes:
+                if g.name == 'Hold ' + c:
+                    return True
+        return False
 
     def get_student_ordering(self, student):
         """
@@ -40,13 +42,14 @@ class Grading(blackboard.grading.Grading):
         indicating how students should be sorted when displayed.
         Typically you want to sort by group, then by name.
         """
-        return (student.group, student.name)
+        return (self.get_student_group_display(student), student.name)
 
     def get_assignment_name_display(self, assignment):
         """
         Return an abbreviation of the name of an assignment.
         """
         if assignment.name.startswith('Week'):
+            # Translate "Week 3 Handin" into "3"
             return assignment.name.split()[1]
         else:
             return assignment.name
@@ -57,6 +60,9 @@ class Grading(blackboard.grading.Grading):
         relating to the given handin.
         """
 
+        # expanduser translates "~" into your home directory
+        base = os.path.expanduser('~/dSik2016')
+
         group_name = attempt.group_name
         if group_name.startswith('Gruppe'):
             class_name = group_name.split()[1]
@@ -64,14 +70,13 @@ class Grading(blackboard.grading.Grading):
         else:
             class_name = group_number = None
 
-        attempt_id = attempt.id
-        if attempt_id.startswith('_'):
-            attempt_id = attempt_id[1:]
-        if attempt_id.endswith('_1'):
-            attempt_id = attempt_id[:-2]
+        attempt_id = re.sub(r'_(.*)_1', r'\1', attempt.id)
+        assignment = self.get_assignment_name_display(attempt.assignment)
+
+        # Translate Handin 1, Class DA2, Group 3, attempt id 456 into
+        # ~/dSik2016/A1-DA2/03-456
         return '{base}/W{assignment}-{class_name}/{group}_{id}'.format(
-            base='/Users/fulnir/dSik2016',
-            assignment=self.get_assignment_name_display(attempt.assignment),
+            base=base, assignment=assignment,
             class_name=class_name, group=group_number, id=attempt_id)
 
     def get_feedback_score(self, comments):
