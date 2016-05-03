8f9ecff463b308c017338f8cab609224c238d7d9
Mathias Rav <rav@cs.au.dk>
2016-05-03 10:02:06 +0200

    Add blackboard.example.emails

---
+++ blackboard/example/emails.py
@@ -0,0 +1,65 @@
+import re
+
+import blackboard
+from blackboard.datatable import fetch_datatable
+from blackboard.backend import fetch_groups
+
+
+def fetch_users(session):
+    url = ('https://bb.au.dk/webapps/blackboard/execute/userManager' +
+           '?course_id=%s' % session.course_id)
+    response, keys, rows = fetch_datatable(session, url)
+    return parse_users(keys, rows)
+
+
+def extract_username(s):
+    s = re.sub(r'^Access the profile card for user: \S+ ', '', s)
+    s = re.sub(r' Remove Users from Course$', '', s)
+    return s
+
+
+def parse_users(keys, rows):
+    first = keys.index('userFirstName')
+    last = keys.index('userLastName')
+    email = keys.index('userEmailAddress')
+    username = keys.index('username')
+    return {
+        extract_username(r[username]):
+        dict(first_name=r[first],
+             last_name=r[last],
+             email=r[email],
+             username=extract_username(r[username]))
+        for r in rows
+    }
+
+
+def fetch_groups_and_emails(session):
+    emails = fetch_users(session)
+    groups = fetch_groups(session)
+    if emails.keys() != groups.keys():
+        print("In Users but not Groups: %r" % (set(emails.keys()) - set(groups.keys())))
+        print("In Groups but not Users: %r" % (set(groups.keys()) - set(emails.keys())))
+        raise Exception("Different username sets")
+    for username in emails.keys():
+        groups[username].update(emails[username])
+    return groups
+
+
+def print_groups_and_emails(session):
+    by_group = {}
+    users = fetch_groups_and_emails(session)
+    for user in users.values():
+        groups = [name for name, group_id in user['groups']]
+        for group in groups or ['no group']:
+            by_group.setdefault(group, []).append(user)
+    for group_name in sorted(by_group.keys()):
+        print("%s:\n" % group_name)
+        group_users = by_group[group_name]
+        group_users = sorted(group_users, key=lambda u: (u['role'], u['first_name'], u['last_name']))
+        for user in group_users:
+            print("\"%s %s\" <%s>," % (user['first_name'], user['last_name'], user['email']))
+        print('')
+
+
+if __name__ == "__main__":
+    blackboard.wrapper(print_groups_and_emails)
