447ffecc034bdf678fa88788e22937f656de3661
Mathias Rav <rav@cs.au.dk>
2017-09-27 11:19:55 +0200

    Fix example/groups.py

--- blackboard/example/groups.py
+++ blackboard/example/groups.py
@@ -4,7 +4,7 @@ from blackboard.backend import fetch_groups
 
 def print_groups(session):
     users = fetch_groups(session)
-    for user in users:
+    for user in users.values():
         print("%s %s (%s, %s) is in %s" %
               (user['first_name'], user['last_name'],
                user['username'], user['role'], user['groups']))
