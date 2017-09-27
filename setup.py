e120f28fab88c65c10d165c3a4187d147ef62375
Mathias Rav <rav@cs.au.dk>
2017-09-27 11:20:00 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.dirname(os.path.abspath(__file__))))
+
+# with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+#     README = readme.read()
+
+setup(
+    name='bbfetch',
+    version='0.1',
+    packages=find_packages(include=['blackboard', 'blackboard.*']),
+    include_package_data=True,
+    license='GNU GPLv3',
+    description='Command-line interface to Blackboard LMS',
+    long_description='Command-line interface to Blackboard LMS',
+    url='https://github.com/Mortal/bbfetch',
+    author='Mathias Rav',
+    author_email='rav@cs.au.dk',
+    install_requires=[
+        'keyring',
+        'requests',
+        'six',
+        'html5lib==0.999999999',
+    ],
+    dependency_links=[
+        'https://github.com/Alir3z4/html2text/archive/2016.1.8.tar.gz#egg=html2text',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
