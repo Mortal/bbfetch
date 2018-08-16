8cc6beb1a7ce54eacdcda22e14add381e68133c6
Mathias Rav <m@git.strova.dk>
2018-08-16 10:32:01 +0200

    WIP: Use aiohttp instead of requests

--- bbfetch/session.py
+++ bbfetch/session.py
@@ -2,11 +2,9 @@ import re
 import getpass
 import keyring
 import html5lib
-import requests
-import requests.cookies
+import aiohttp
 
-from six.moves.http_cookiejar import LWPCookieJar
-from six.moves.urllib.parse import urlparse, parse_qs, urlencode
+from six.moves.urllib.parse import urlencode
 
 from bbfetch.base import BadAuth, ParserError, logger, DOMAIN
 
@@ -21,26 +19,31 @@ class BlackboardSession:
         self.course_id = course_id
 
         self.password = None
-        self.cookies = LWPCookieJar(cookiejar)
-        self.session = requests.Session()
+        self.cookies = aiohttp.CookieJar()
+        self.session = aiohttp.ClientSession(cookie_jar=self.cookies)
         self.load_cookies()
+
+    def __aenter__(self):
+        return self.session.__aenter__()
+
+    def __aexit__(self, *args):
+        return self.session.__aexit__(*args)
 
     def load_cookies(self):
         try:
-            self.cookies.load(ignore_discard=True)
+            self.cookies.load(self.cookiejar_filename)
         except FileNotFoundError:
             pass
-        requests.cookies.merge_cookies(self.session.cookies, self.cookies)
 
     def save_cookies(self):
-        requests.cookies.merge_cookies(self.cookies, self.session.cookies)
-        self.cookies.save(ignore_discard=True)
+        self.cookies.save(self.cookiejar_filename)
 
     def get_cookie(self, key, path):
+        cookies = self.cookies.filter_cookies('https://' + DOMAIN + path)
         try:
-            return self.session.cookies._cookies[DOMAIN][path][key].value
+            return cookies[key].value
         except KeyError:
-            print(self.session.cookies._cookies)
+            print(self.cookies)
             raise
 
     def get_username(self):
@@ -67,7 +70,7 @@ class BlackboardSession:
             raise ValueError("forget_password: username is None")
         keyring.delete_password("fetch.py WAYF", self.username)
 
-    def wayf_login(self, response):
+    async def wayf_login(self, response):
         """Login to WAYF.
 
         Parameters
@@ -78,31 +81,31 @@ class BlackboardSession:
 
         history = list(response.history) + [response]
         logger.info("Sending login details to WAYF")
-        response = self.session.post(response.url, self.get_auth())
+        response = await self.session.post(response.url, data=self.get_auth())
         history += list(response.history) + [response]
-        if 'Forkert brugernavn eller kodeord' in response.text:
+        if 'Forkert brugernavn eller kodeord' in await response.text():
             raise BadAuth()
 
-        response = self.post_hidden_form(response)
+        response = await self.post_hidden_form(response)
         history += list(response.history) + [response]
-        response = self.post_hidden_form(response)
+        response = await self.post_hidden_form(response)
         history += list(response.history) + [response]
-        response.history = history[:-1]
+        response._history = history[:-1]
         return response
 
-    def relogin(self):
+    async def relogin(self):
         url = (
             'https://%s/webapps/bb-auth-provider-shibboleth-BBLEARN' % DOMAIN +
             '/execute/shibbolethLogin?authProviderId=_102_1')
-        response = self.get(url)
-        if self.detect_login(response) is False:
+        response = await self.get(url)
+        if self.detect_login(await response.text()) is False:
             logger.error("Seems logged out after re-login. " +
                          "Try deleting your cookiejar.")
             raise ParserError("Not logged in", response)
         return response
 
-    def detect_login(self, response):
-        document = html5lib.parse(response.content, transport_encoding=response.encoding)
+    def detect_login(self, response_text):
+        document = html5lib.parse(response_text)
         log_in_id = 'topframe.login.label'
         o = document.find('.//h:a[@id="%s"]' % log_in_id, NS)
         if o is not None:
@@ -112,7 +115,7 @@ class BlackboardSession:
         if o is not None:
             return True
 
-    def post_hidden_form(self, response):
+    async def post_hidden_form(self, response):
         """Send POST request to form with only hidden fields.
 
         Parameters
@@ -121,7 +124,7 @@ class BlackboardSession:
             Page containing form with only hidden fields
         """
 
-        document = html5lib.parse(response.content, transport_encoding=response.encoding)
+        document = html5lib.parse(await response.text())
         form = document.find('.//h:form', NS)
         url = form.get('action')
         inputs = form.findall('.//h:input[@name]', NS)
@@ -131,11 +134,11 @@ class BlackboardSession:
             i.get('name'): i.get('value')
             for i in inputs
         }
-        response = self.session.post(url, post_data)
+        response = await self.session.post(url, data=post_data)
 
         return response
 
-    def follow_html_redirect(self, response):
+    async def follow_html_redirect(self, response):
         """Repeatedly follow HTML redirects in the page.
 
         If the given response has no HTML redirect, return it unaltered.
@@ -155,7 +158,7 @@ class BlackboardSession:
 
         while True:
             document = html5lib.parse(
-                response.content, transport_encoding=response.encoding)
+                await response.text())
             scripts = document.findall('.//h:script', NS)
 
             next_url = None
@@ -166,10 +169,9 @@ class BlackboardSession:
                     next_url = mo.group('url')
                     break
             if next_url is not None:
-                o = urlparse(next_url)
-                p = o.netloc + o.path
+                p = next_url.host + next_url.path
                 if p == '%s/webapps/login/' % DOMAIN:
-                    qs = parse_qs(o.query)
+                    qs = next_url.query
                     try:
                         return_url = qs['new_loc'][0]
                     except KeyError:
@@ -185,14 +187,14 @@ class BlackboardSession:
                         dict(returnUrl=return_url,
                              authProviderId='_102_1'))
                     next_url = '%s?%s' % (real_login_url, new_qs)
-                response = self.session.get(next_url)
+                response = await self.session.get(next_url)
                 history += list(response.history) + [response]
                 continue
             break
-        response.history = history[:-1]
+        response._history = history[:-1]
         return response
 
-    def autologin(self, response):
+    async def autologin(self, response):
         """Automatically log in if necessary.
 
         If the given response is not for a login form,
@@ -200,49 +202,48 @@ class BlackboardSession:
         Otherwise, log in using wayf_login and get_auth.
         """
 
-        response = self.follow_html_redirect(response)
-        o = urlparse(response.url)
-        if o.netloc == 'wayf.au.dk':
-            response = self.wayf_login(response)
+        response = await self.follow_html_redirect(response)
+        if response.url.host == 'wayf.au.dk':
+            response = await self.wayf_login(response)
         return response
 
-    def get_edit_mode(self, response):
-        document = html5lib.parse(response.content, transport_encoding=response.encoding)
+    def get_edit_mode(self, response_text):
+        document = html5lib.parse(response_text)
         mode_switch = document.find('.//*[@id="editModeToggleLink"]', NS)
         if mode_switch is not None:
             return 'read-on' in (mode_switch.get('class') or '').split()
 
-    def ensure_edit_mode(self, response):
-        if self.get_edit_mode(response) is False:
+    async def ensure_edit_mode(self, response):
+        if self.get_edit_mode(await response.text()) is False:
             url = ('https://%s/webapps/blackboard/execute/' % DOMAIN +
                    'doCourseMenuAction?cmd=setDesignerParticipantViewMode' +
                    '&courseId=' + self.course_id +
                    '&mode=designer')
             logger.debug("Switch to edit mode")
-            r = self.get(url)
+            r = await self.get(url)
             history = (list(response.history) + [response] +
                        list(r.history) + [r])
-            response = self.get(history[0].url)
-            response.history = history + list(response.history)
+            response = await self.get(history[0].url)
+            response._history = history + list(response.history)
         return response
 
-    def get(self, url):
-        response = self.autologin(self.session.get(url))
-        if self.detect_login(response) is False:
+    async def get(self, url):
+        response = await self.autologin(await self.session.get(url))
+        if self.detect_login(await response.text()) is False:
             history = response.history + [response]
-            relogin_response = self.relogin()
+            relogin_response = await self.relogin()
             history += relogin_response.history + [relogin_response]
-            response = self.autologin(self.session.get(url))
-            response.history = history + list(response.history)
+            response = await self.autologin(await self.session.get(url))
+            response._history = history + list(response.history)
         if response.url != url:
             history = list(response.history) + [response]
-            response = self.session.get(url)
-            response.history = history + list(response.history)
-        self.log_error(response)
+            response = await self.session.get(url)
+            response._history = history + list(response.history)
+        self.log_error(await response.text())
         return response
 
-    def log_error(self, response):
-        document = html5lib.parse(response.content, transport_encoding=response.encoding)
+    def log_error(self, response_text):
+        document = html5lib.parse(response_text)
         content = document.find('.//h:div[@id="contentPanel"]', NS)
         if content is not None:
             class_list = (content.get('class') or '').split()
@@ -250,8 +251,8 @@ class BlackboardSession:
                 logger.info("contentPanel indicates an error has occurred")
                 # raise ParserError("Error", response)
 
-    def post(self, url, data, files=None, headers=None):
-        response = self.session.post(
+    async def post(self, url, data, files=None, headers=None):
+        response = await self.session.post(
             url, data=data, files=files, headers=headers)
         # if response.history:
         #     logger.warning('POST %r redirected', url)
@@ -260,12 +261,13 @@ class BlackboardSession:
         #     logger.warning("... to %r", response.url)
         return response
 
-    def ensure_logged_in(self):
+    async def ensure_logged_in(self):
         url = (
             'https://%s/webapps/blackboard/content/manageDashboard.jsp' % DOMAIN +
             '?course_id=%s' % self.course_id +
             '&sortCol=LastLoginCol&sortDir=D')
-        self.get(url)
+        async with (await self.get(url)):
+            pass
 
 
 class PassBlackboardSession(BlackboardSession):
